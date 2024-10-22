e9f94e3cda281d3b20707945072ccbcc4a448f1f
Madhav Solanki <madhav09solanki@gmail.com>
2024-10-22 16:29:45 +0530

    Portfolio Completed

---
+++ src/data/constants.js
@@ -0,0 +1,440 @@
+export const Bio = {
+    name: "Madhav Solanki",
+    roles: [
+        "Backend Developer",
+        "Android Developer",
+        "Spring-Boot Developer",
+    ],
+    description:
+        "As a driven and adaptable professional, I thrive on challenges and continuously seek opportunities for growth. My passion for learning fuels my ability to quickly master new skills and technologies, allowing me to deliver innovative solutions and exceed expectations. With a positive mindset and unwavering commitment to excellence, I approach each project as a chance to make a meaningful impact. My versatility, coupled with strong problem-solving skills, enables me to contribute effectively in dynamic environments and drive success in collaborative efforts.",
+    github: "https://github.com/madhavsolanki",
+    resume:
+        "https://drive.google.com/file/d/1KhQhn_Zrgq8dTtzD3IzULJl2C3fZhrNS/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/madhavsolanki21/",
+    twitter: "https://x.com/Madhav_2101?t=_HsGmsO1ah1BaFCTKqC7yg&s=09",
+    insta: "https://www.instagram.com/chunni_laal_21/profilecard/?igsh=MTFpOXNjcmhmZGRy",
+};
+
+export const skills = [
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "HTML",
+                image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+            },
+            {
+                name: "CSS",
+                image:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+            },
+            {
+                name: "JavaScript",
+                image:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+            },
+            {
+                name: "React Js",
+                image:
+                    "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+            },
+            {
+                name: "Redux",
+                image:
+                    "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+            },
+            {
+                name: "Bootstrap",
+                image:
+                    "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            },
+            {
+                name: "Tailwind CSS",
+                image:
+                    "https://res.cloudinary.com/itshirdeshk/image/upload/v1721044882/Tailwind_CSS_r1pbul.png",
+            },
+            {
+                name: "Material UI",
+                image:
+                    "https://icon.icepanel.io/Technology/svg/Material-UI.svg",
+            },
+            {
+                name: "Shadcn",
+                image:
+                    "https://seeklogo.com/images/S/shadcn-ui-logo-EF735EC0E5-seeklogo.com.png?v=638421451470000000",
+            },
+        
+        ],
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "Spring Boot",
+                image:
+                    "https://e4developer.com/wp-content/uploads/2018/01/spring-boot.png",
+            },
+            {
+                name: "Node Js",
+                image: "https://icon.icepanel.io/Technology/svg/Node.js.svg",
+            },
+            {
+                name: "Express Js",
+                image:
+                    "https://upload.vectorlogo.zone/logos/expressjs/images/a1b5cb1f-dae7-4971-ab5b-68efce751b0f.svg",
+            },
+
+            // {
+            //     name: "Graph Ql",
+            //     image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+            // },
+            
+            {
+                name: "MySQL",
+                image:
+                    "https://icon.icepanel.io/Technology/svg/MySQL.svg",
+            },
+            // {
+            //     name: "Postgresql",
+            //     image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+            // },
+            {
+                name: "MongoDB",
+                image:
+                    "https://icon.icepanel.io/Technology/svg/MongoDB.svg",
+            },
+            {
+                name: "Firebase",
+                image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+            },
+            // {
+            //     name: "Supabase",
+            //     image: "https://www.vectorlogo.zone/logos/supabase/supabase-icon.svg",
+            // },
+            {
+                name: "Appwrite",
+                image: "https://icon.icepanel.io/Technology/svg/Appwrite.svg",
+            },
+            {
+                name: "Redis",
+                image: "https://icon.icepanel.io/Technology/svg/Redis.svg",
+            },
+            {
+                name: "Kafka",
+                image: "https://www.vectorlogo.zone/logos/apache_kafka/apache_kafka-icon.svg",
+            },
+        
+        ],
+    },
+    {
+        title: "Android",
+        skills: [
+            {
+                name: "Java",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+            },
+            {
+                name: "Kotlin",
+                image:
+                    "https://th.bing.com/th/id/OIP.0md50ilOuWsS2XUNNbzfhwHaHa?rs=1&pid=ImgDetMain",
+            },
+            {
+                name: "XML",
+                image:
+                    "https://icon.icepanel.io/Technology/svg/XML.svg",
+            },
+            {
+                name: "Jetpack Compose",
+                image:
+                    "https://th.bing.com/th/id/OIP.AYW0kcLCdQYNZUtbiLBqvAHaIA?rs=1&pid=ImgDetMain",
+            },
+            {
+                name: "Android Studio",
+                image:
+                    "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+            },
+        ],
+    },
+    {
+        title: "Others",
+        skills: [
+            {
+                name: "Git",
+                image:
+                    "https://icon.icepanel.io/Technology/svg/Git.svg",
+            },
+            {
+                name: "GitHub",
+                image:
+                    "https://www.vectorlogo.zone/logos/github/github-tile.svg",
+            },
+            {
+                name: "Docker",
+                image:
+                    "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+            },
+            {
+                name: "Vercel",
+                image:
+                    "https://icon.icepanel.io/Technology/png-shadow-512/Vercel.png",
+            },
+            {
+                name: "Netlify",
+                image:
+                    "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+            },
+            {
+                name: "VS Code",
+                image:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+            },
+            {
+                name: "Postman",
+                image:
+                    "https://icon.icepanel.io/Technology/svg/Postman.svg",
+            },
+            {
+                name: "Figma",
+                image:
+                    "https://icon.icepanel.io/Technology/svg/Figma.svg",
+            },
+        ],
+    },
+];
+
+export const experiences = [
+    {
+        id: 0,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXAeQf3EwFBzk7X695p73YCH8Lm2onldqoQ&s",
+        role: "Brainwave Matrix Solutions",
+        company: "Web Development Intern",
+        date: "Aug 2024 - Sep 2024",
+        desc: "Worked on different web development projects.",
+        // doc: "https://res.cloudinary.com/itshirdeshk/image/upload/v1726931908/Brainwave_hmif8n.jpg",
+    },
+    {
+        id: 1,
+        img: "https://res.cloudinary.com/itshirdeshk/image/upload/v1726930771/Headstarter_qamq3t.jpg",
+        role: "SWE Fellowship",
+        company: "Headstarter AI",
+        date: "May 2024 - July 2024",
+        desc: "Headstarter Summer Fellow. This is a 7-week software engineering fellowship. The program will consist of building 5 AI projects, 5 weekend hackathons, 1 final project with 1000+ users, interview prep, resume reviews and feedback from real software engineers.",
+        // doc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFv-4PfkPZZuRTe3aboRo9M8MhqDruiZq2fw&s",
+    },
+    // {
+    //     id: 3,
+    //     img: "https://avatars.githubusercontent.com/u/39153675?s=280&v=4",
+    //     role: "Open Source Contributor",
+    //     company: "GirlScript Summer of Code",
+    //     date: "May 2024 - Present",
+    //     desc: "Contributing to different open-source projects.",
+    //     doc: "https://res.cloudinary.com/itshirdeshk/image/upload/v1721051177/contributor_badge_2024_tgovmm.png",
+    // },
+    {
+        id: 2,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpXFkM6OLkGL2A33dpKVBjSKSq9b9wO1FXGg&s",
+        role: "Open Source Contributor",
+        company: "Social Summer of Code",
+        date: "May 2024 - Present",
+        desc: "Contributing to different open-source projects.",
+        // doc: "https://res.cloudinary.com/itshirdeshk/image/upload/v1721051178/SSOC_Badge_s6sq03.png",
+    },
+];
+
+export const education = [
+    {
+        id: 0,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5xh-TiwOBmk8xZSPC_br_loloibcEupp5MQ&s",
+        school: "Raja Balwant Singh Engineering Technical Campus",
+        date: "Sep 2021 - Jul 2025",
+        grade: "7.5 CGPA",
+        desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Raja Balwant Singh Engineering Technical Campus, Agra. I have completed 6 semesters and have a CGPA of 8.0. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+        degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+        id: 1,
+        img: "https://wpsaligarh.in/wpsweb/images/wps/logo.png",
+        school: "Wisdom Public School",
+        date: "Apr 2020 - Mar 2021",
+        grade: "84.2%",
+        desc: "I completed my class 12 education at Wisdom Public School, Aligarh, where I studied PCM with Physical Education.",
+        degree: "CBSE(XII)",
+    },
+    {
+        id: 2,
+        img: "https://wpsaligarh.in/wpsweb/images/wps/logo.png",
+        school: "Wisdom Public School",
+        date: "Apr 2018 - Mar 2019",
+        grade: "80.6%",
+        desc: "I completed my class 10 education at Wisdom Public School, Aligarh, where I studied Science with Computer Application.",
+        degree: "CBSE(X), Science with Computer",
+    },
+];
+
+export const projects = [
+    {
+        id: 0,
+        title: "Trimrr",
+        description:
+            "Trimrr is an url shortner app that can shorten your long unreadable url into customized readable url.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955595/Screenshot_248_rid8u6.png",
+        tags: [
+            "React Js",
+            "Shadcn Ui",
+            "Tailwind Css",
+            "Supabase"
+        ],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Trimrr",
+        link: "https://trimrr-sable.vercel.app/",
+    },
+    {
+        id: 1,
+        title: "Mystery Messenger",
+        description:
+            "Mystery Messenger lets you message anyone anything anonymously.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1726932572/Screenshot_424_p58hcc.png",
+        tags: [
+            "Next Js",
+            "Shadcn Ui",
+            "Tailwind Css",
+            "MongoDB",
+            "Vercel AI SDK",
+            "Gemini API"
+        ],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Mystery-Messenger",
+        link: "https://mystery-messenger-six.vercel.app/",
+    },
+    {
+        id: 2,
+        title: "Crypted Vault",
+        description:
+            "Crypted Vault lets you to upload and store image with web3 security.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1726937896/Screenshot_438_ahmnks.png",
+        tags: [
+            "React Js",
+            "Nodejs",
+            "Expressjs",
+            "MongoDB",
+            "Tailwind Css",
+            "Solidity",
+        ],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Crypto-Vault",
+        link: "https://crypto-vault-self.vercel.app/",
+    },
+    {
+        id: 3,
+        title: "Chatting Hub",
+        description:
+            "Chatting Hub is a full stack web application where you can chat with your friends in realtime.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955594/Screenshot_62_crgud1.png",
+        tags: ["React Js", "Node Js", "Express Js", "MongoDb", "Socket.io"],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Backend-Projects/tree/main/Chatting-Hub",
+        link: "https://chatting-hub.vercel.app/",
+    },
+    {
+        id: 2,
+        title: "Pathfinding Visualizer",
+        description:
+            "Pathfinfing Visualizer helps you to visualize the path made by various path based algorithms like DFS, BFS, Dijkstra, A-star to reach the destination.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955594/Screenshot_247_yxlak6.png",
+        tags: [
+            "React Js",
+            "Typescript",
+            "Tailwind Css"
+        ],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Pathfinding-Visualizer",
+        link: "https://pathfinding-visualizer-plum.vercel.app/",
+    },
+    {
+        id: 4,
+        title: "Notes Hub",
+        description:
+            "Notes Hub is simple to use free note taking app made with NodeJs, EJS and MongoDB.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955595/Screenshot_53_d9bzp5.png",
+        tags: ["Node Js", "EJS", "MongoDb"],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Backend-Projects/tree/main/Notes-Hub",
+        link: "https://notes-hub-ufwa.onrender.com/",
+    },
+    {
+        id: 6,
+        title: "Video Calling App",
+        description:
+            "By using Video Calling App, you can create a room where you can video call with the other person that joined the room.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955594/Screenshot_83_lnundz.png",
+        tags: ["React Js", "WebRTC", "WebSocket", "Socket.io"],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Backend-Practice/tree/main/WebRTC%20-%202",
+        link: "https://backend-practice-nine.vercel.app/",
+    },
+    {
+        id: 7,
+        title: "Realtime Tracker",
+        description:
+            "Realtime Tracker helps you to track the location of whom that connected on the app.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955596/Screenshot_201_itsuyf.png",
+        tags: ["EJS", "CSS", "Node Js", "WebSocket", "Socket.io"],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Backend-Projects/tree/main/Realtime-Tracker",
+        link: "https://realtime-tracker-76ek.onrender.com",
+    },
+    {
+        id: 8,
+        title: "Pin Lite",
+        description:
+            "Pin Lite helps you to create pins where you can upload your images.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955594/Screenshot_35_mnm9qk.png",
+        tags: ["EJS", "CSS", "MongoDb", "Passport Js"],
+        category: "web app",
+        github: "https://github.com/itshirdeshk/Backend-Projects/tree/main/Pin-Lite",
+        link: "https://pin-lite.onrender.com/",
+    },
+    {
+        id: 9,
+        title: "SMS Spam Classifier",
+        description:
+            "SMS Spam Classifier classify your sms, email or messages as spam or not.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1720955593/Screenshot_4_h1tcxd.png",
+        tags: ["Python", "Numpy", "Pandas", "Keras", "NLTK", "Matplotlib", "Naive Bayes", "Scikit-Learn", "Streamlit"],
+        category: "machine learning",
+        github: "https://github.com/itshirdeshk/SMS-Spam-Classifier",
+    },
+    {
+        id: 15,
+        title: "Allen",
+        description:
+            "Allen is a voice assistant app that can give answers to your questions.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1721043137/Allen_eenyab.jpg",
+        tags: ["Flutter", "OpenAi Api"],
+        category: "android app",
+        github: "https://github.com/itshirdeshk/Flutter-Projects/tree/master/allen",
+    },
+    {
+        id: 16,
+        title: "My Gpt",
+        description:
+            "My Gpt helps you to ask questions and then it will give answers to your questions.",
+        image:
+            "https://res.cloudinary.com/itshirdeshk/image/upload/v1721042910/MyGpt_nb86a9_Sharpened_ky4wjp.jpg",
+        tags: ["Flutter", "Provider", "OpenAi Api"],
+        category: "android app",
+        github: "https://github.com/itshirdeshk/my__gpt",
+    },
+];
